9ed56bf50625ea53c03c95ab6b0ca23dbb9aa133
Phu Ngo Kim <kimphu_2408@yahoo.com.vn>
2021-09-07 17:44:03 +0800

    Promote to oh-my-zsh plugin
    copied from https://github.com/ohmyzsh/ohmyzsh/blob/8634d95/plugins/git-escape-magic/git-escape-magic.plugin.zsh

--- git-escape-magic.plugin.zsh
+++ git-escape-magic.plugin.zsh
@@ -1,3 +1,9 @@
-source ${0:A:h}/git-escape-magic
+# Automatically detect and escape zsh globbing meta-characters when used with
+# git refspec characters like `[^~{}]`. NOTE: This must be loaded _after_
+# url-quote-magic.
+#
+# This trick is detailed at https://github.com/knu/zsh-git-escape-magic and is
+# what allowed this plugin to exist.
+
 autoload -U git-escape-magic
 git-escape-magic
